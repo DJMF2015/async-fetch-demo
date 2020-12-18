3b5c4d2a10436bbb12544a848df72f79f72d2260
djmf2020 <david@passion4social.com>
2020-12-18 12:52:20 +0000

    added post request method. Need to shift json data to localhost for post to work correctly

--- fetch_practice/index2.js
+++ fetch_practice/index2.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (const q of data) {
             //Find the container where we attach everything to 
             const quoteUL = document.querySelector('#quote-list');
+            //form variable for holding data to post
             const form = document.querySelector('#new-quote-form');
             //Create all necessary elements
             const quoteLi = document.createElement('li');
@@ -19,21 +20,22 @@ document.addEventListener('DOMContentLoaded', () => {
             const br = document.createElement('br');
             const hr = document.createElement('hr') //Add appropriate classes and ids. Grab data and insert if needed.
 
-
             //create 'dislike' button
             const dislikesBtn = document.createElement('button');
 
-            quoteLi.className = 'quote-card';
+            quoteLi.className = 'quote-card'; //for styling
             blockQuote.className = 'blockquote';   //for styling
             para.className = 'mb-1';                      //for styling
-            quoteLi.className = 'quote-card';
+            quoteLi.className = 'quote-card';  //for styling
 
             footer.className = 'blockquote-footer';    //for styling
             quoteLi.dataset.id = q.id   //Grab data and insert it into created elements  
 
             id.innerHTML = q.id;
             para.innerHTML = q.body;
-            footer.innerHTML = q.title;    //Append everything to main container
+            footer.innerHTML = q.title;
+
+            //Append everything to main container
 
             //attach dislike button to body quote
 
@@ -49,19 +51,41 @@ document.addEventListener('DOMContentLoaded', () => {
             /*1. Run fetch function with the appropriate url and required object argument when running a delete fetch request. This will delete on backend.
             2. After running delete fetch request, simply delete quote node on frontend.*/
 
-            // form.addEventListener('submit', (e) => postQuote(e))
+            //POST DATA: attach eventlistener to form to handle request
+            // const form = document.querySelector('#new-quote-form');
+            form.addEventListener('submit', (e) => postQuote(e));
 
-
+            function postQuote(e) {
+                e.preventDefault();
+                //grab form field data to send to the backend.
+                const newQuote = document.querySelector('#new-quote').value;
+                const newAuthor = document.querySelector('#author').value;
+                //send to backend
+                const url = `https://jsonplaceholder.typicode.com/posts/`
+                // const url = 'http://localhost:3000/quotes';!
+                const reqObject = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        title: newQuote,
+                        body: newAuthor
+                    })
+                }
+                fetchData(url, reqObject)
+                    .then(resp => resp.json())
+                    .then(quote => renderQuotes([quote]))
+            }
+            
             function deleteQuote() {
                 const url = `https://jsonplaceholder.typicode.com/posts/${q.id}`
                 const reqObject = {
                     method: 'DELETE'
                 };
                 fetch(url, reqObject)
-                .then(quoteLi.remove());
+                    .then(quoteLi.remove());
                 //note. Running a delete fetch request is different from a get request.
-             console.log(url )
-            } 
+                console.log(url)
+            }
         }
     }//Call the function that will automatically run renderQuote() also
     fetchData();
